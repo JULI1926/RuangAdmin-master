7f4c7578543ffd27770af6fa975322657c16c157
= <=>
2024-08-30 21:13:36 -0500

    Plantilla de Ruan Admin Template Bootstrap

---
+++ config/slidebar.php
@@ -0,0 +1,47 @@
+<ul class="navbar-nav sidebar sidebar-light accordion" id="accordionSidebar">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon">
+            <img src="img/logo/logo2.png">
+        </div>
+        <div class="sidebar-brand-text mx-3">SGIA Admin</div>
+    </a>
+    
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#lista1"
+            aria-expanded="true" aria-controls="lista1">
+            <i class="far fa-fw fa-window-maximize"></i>
+            <span>Lista 1</span>
+        </a>
+        <div id="lista1" class="collapse" aria-labelledby="headingBootstrap" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Bootstrap UI</h6>
+                <a class="collapse-item" href="alerts.html">Alerts</a>
+                <a class="collapse-item" href="buttons.html">Buttons</a>
+                <a class="collapse-item" href="dropdowns.html">Dropdowns</a>
+                <a class="collapse-item" href="modals.html">Modals</a>
+                <a class="collapse-item" href="popovers.html">Popovers</a>
+                <a class="collapse-item" href="progress-bar.html">Progress Bars</a>
+            </div>
+        </div>
+    </li> 
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#lista2"
+            aria-expanded="true" aria-controls="lista2">
+            <i class="far fa-fw fa-window-maximize"></i>
+            <span>Lista 2</span>
+        </a>
+        <div id="lista2" class="collapse" aria-labelledby="headingBootstrap" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Bootstrap UI</h6>
+                <a class="collapse-item" href="alerts.html">Alerts</a>
+                <a class="collapse-item" href="buttons.html">Buttons</a>
+                <a class="collapse-item" href="dropdowns.html">Dropdowns</a>
+                <a class="collapse-item" href="modals.html">Modals</a>
+                <a class="collapse-item" href="popovers.html">Popovers</a>
+                <a class="collapse-item" href="progress-bar.html">Progress Bars</a>
+            </div>
+        </div>
+    </lilista2   
+    <hr class="sidebar-divider">
+    <div class="version" id="version-ruangadmin"></div>
+</ul>
---
+++ index.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php include('config/head.php'); ?>
+</head>
+
+<body id="page-top">
+  <div id="wrapper">
+    <!-- Sidebar -->
+    <?php include('config/slidebar.php'); ?>
+    </head>
+    <!-- Sidebar -->
+    <div id="content-wrapper" class="d-flex flex-column">
+      <div id="content">
+        <!-- TopBar -->
+        <?php include('config/topbar.php'); ?>
+        <!-- Topbar -->
+
+        <!-- Container Fluid-->
+        <div class="container-fluid" id="container-wrapper">
+          <?php include('config/ruta.php'); ?>
+
+          <div class="row">
+            <div class="col-lg-12 col-sm-12 col-md-8 col-lg-6 col-xl-4">
+              <div class="card mb-4">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Basic Buttons</h6>
+                </div>
+                <div class="card-body">
+                  <p>Bootstrap includes several predefined button styles, each serving its own
+                    semantic purpose, with a
+                    few extras thrown in for more control.</p>
+                  <button type="button" class="btn btn-primary mb-1">Primary</button>
+                  <button type="button" class="btn btn-secondary mb-1">Secondary</button>
+                  <button type="button" class="btn btn-success mb-1">Success</button>
+                  <button type="button" class="btn btn-danger mb-1">Danger</button>
+                  <button type="button" class="btn btn-warning mb-1">Warning</button>
+                  <button type="button" class="btn btn-info mb-1">Info</button>
+                  <button type="button" class="btn btn-light mb-1">Light</button>
+                  <button type="button" class="btn btn-dark mb-1">Dark</button>
+                  <button type="button" class="btn btn-link">Link</button>
+                </div>
+              </div>              
+            </div>
+            <div class="col-lg-12 col-sm-12 col-md-8 col-lg-6 col-xl-4">
+              <div class="card mb-4">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Basic Buttons</h6>
+                </div>
+                <div class="card-body">
+                  <p>Bootstrap includes several predefined button styles, each serving its own
+                    semantic purpose, with a
+                    few extras thrown in for more control.</p>
+                  <button type="button" class="btn btn-primary mb-1">Primary</button>
+                  <button type="button" class="btn btn-secondary mb-1">Secondary</button>
+                  <button type="button" class="btn btn-success mb-1">Success</button>
+                  <button type="button" class="btn btn-danger mb-1">Danger</button>
+                  <button type="button" class="btn btn-warning mb-1">Warning</button>
+                  <button type="button" class="btn btn-info mb-1">Info</button>
+                  <button type="button" class="btn btn-light mb-1">Light</button>
+                  <button type="button" class="btn btn-dark mb-1">Dark</button>
+                  <button type="button" class="btn btn-link">Link</button>
+                </div>
+              </div>              
+            </div>
+            <div class="col-lg-12 col-sm-12 col-md-8 col-lg-6 col-xl-4">
+              <div class="card mb-4">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Basic Buttons</h6>
+                </div>
+                <div class="card-body">
+                  <p>Bootstrap includes several predefined button styles, each serving its own
+                    semantic purpose, with a
+                    few extras thrown in for more control.</p>
+                  <button type="button" class="btn btn-primary mb-1">Primary</button>
+                  <button type="button" class="btn btn-secondary mb-1">Secondary</button>
+                  <button type="button" class="btn btn-success mb-1">Success</button>
+                  <button type="button" class="btn btn-danger mb-1">Danger</button>
+                  <button type="button" class="btn btn-warning mb-1">Warning</button>
+                  <button type="button" class="btn btn-info mb-1">Info</button>
+                  <button type="button" class="btn btn-light mb-1">Light</button>
+                  <button type="button" class="btn btn-dark mb-1">Dark</button>
+                  <button type="button" class="btn btn-link">Link</button>
+                </div>
+              </div>              
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-lg-12 col-sm-12 col-md-8 col-lg-6 col-xl-4">
+              <div class="card mb-4">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Basic Buttons</h6>
+                </div>
+                <div class="card-body">
+                  <p>Bootstrap includes several predefined button styles, each serving its own
+                    semantic purpose, with a
+                    few extras thrown in for more control.</p>
+                  <button type="button" class="btn btn-primary mb-1">Primary</button>
+                  <button type="button" class="btn btn-secondary mb-1">Secondary</button>
+                  <button type="button" class="btn btn-success mb-1">Success</button>
+                  <button type="button" class="btn btn-danger mb-1">Danger</button>
+                  <button type="button" class="btn btn-warning mb-1">Warning</button>
+                  <button type="button" class="btn btn-info mb-1">Info</button>
+                  <button type="button" class="btn btn-light mb-1">Light</button>
+                  <button type="button" class="btn btn-dark mb-1">Dark</button>
+                  <button type="button" class="btn btn-link">Link</button>
+                </div>
+              </div>              
+            </div>
+            <div class="col-lg-12 col-sm-12 col-md-8 col-lg-6 col-xl-4">
+              <div class="card mb-4">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Basic Buttons</h6>
+                </div>
+                <div class="card-body">
+                  <p>Bootstrap includes several predefined button styles, each serving its own
+                    semantic purpose, with a
+                    few extras thrown in for more control.</p>
+                  <button type="button" class="btn btn-primary mb-1">Primary</button>
+                  <button type="button" class="btn btn-secondary mb-1">Secondary</button>
+                  <button type="button" class="btn btn-success mb-1">Success</button>
+                  <button type="button" class="btn btn-danger mb-1">Danger</button>
+                  <button type="button" class="btn btn-warning mb-1">Warning</button>
+                  <button type="button" class="btn btn-info mb-1">Info</button>
+                  <button type="button" class="btn btn-light mb-1">Light</button>
+                  <button type="button" class="btn btn-dark mb-1">Dark</button>
+                  <button type="button" class="btn btn-link">Link</button>
+                </div>
+              </div>              
+            </div>
+            <div class="col-lg-12 col-sm-12 col-md-8 col-lg-6 col-xl-4">
+              <div class="card mb-4">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Basic Buttons</h6>
+                </div>
+                <div class="card-body">
+                  <p>Bootstrap includes several predefined button styles, each serving its own
+                    semantic purpose, with a
+                    few extras thrown in for more control.</p>
+                  <button type="button" class="btn btn-primary mb-1">Primary</button>
+                  <button type="button" class="btn btn-secondary mb-1">Secondary</button>
+                  <button type="button" class="btn btn-success mb-1">Success</button>
+                  <button type="button" class="btn btn-danger mb-1">Danger</button>
+                  <button type="button" class="btn btn-warning mb-1">Warning</button>
+                  <button type="button" class="btn btn-info mb-1">Info</button>
+                  <button type="button" class="btn btn-light mb-1">Light</button>
+                  <button type="button" class="btn btn-dark mb-1">Dark</button>
+                  <button type="button" class="btn btn-link">Link</button>
+                </div>
+              </div>              
+            </div>
+          </div>
+
+          <!-- Modal Logout -->
+          <?php include('config/modal.php'); ?>
+
+        </div>
+        <!---Container Fluid-->
+      </div>
+      <!-- Footer -->
+      <?php include('config/footer.php'); ?>
+      <!-- Footer -->
+    </div>
+  </div>
+
+  <!-- Scroll to top -->
+  <?php include('config/toTop.php'); ?>
+  <?php include('config/scripts.php'); ?>
+</body>
+
+</html>
